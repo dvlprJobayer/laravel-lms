bef1fcc6aca0d1ff6421d4f4c102347dcefaa3ba
dvlprJobayer <nesarahammed019@gmail.com>
2023-01-05 23:02:56 +0600

    part 01 completed

---
+++ database/migrations/2023_01_05_154843_create_home_works_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home_works', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->text('link');
+            $table->foreignId('curriculum_id')->default(0)->constrained()->onDelete('cascade');
+            $table->foreignId('exam_id')->default(0)->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home_works');
+    }
+};
